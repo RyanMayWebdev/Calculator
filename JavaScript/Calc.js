4d9d54e07e8f9e7f6b255e0f466a0fcf495fd40b
Ryan May <ryan777may@gmail.com>
2021-08-02 14:08:55 -0400

    Add keydown listener, now you can use your kyboard for calculating. ooo ahhh

--- JavaScript/Calc.js
+++ JavaScript/Calc.js
@@ -25,11 +25,11 @@ $(document).ready(function () {
         $output.text($log);
     };
 
+    //perform calculation using selected operator and numbers.
     const $calculate = () => {
         $firstNum = parseFloat($firstNum);
         $secondNum = parseFloat($secondNum);
-        console.log($firstNum);
-        console.log($secondNum)
+
         switch ($operator) {
             case '+':
                 $output.html(`<p>${$add($firstNum, $secondNum)}</p>`);
@@ -47,29 +47,73 @@ $(document).ready(function () {
             default:
                 break;
         }
-        console.log('this ran')
+
         $firstNum = $output.text()
         $secondNum = ''
         $operator = ''
     }
 
-    $('button').on('click',function(){
-        this.classList.add('btn-clicked')
-        setTimeout(()=>this.classList.remove('btn-clicked'),200)
+    const $flash = (value) => {
+        let $button = $("button[value='" + value + "']")
+        $button.addClass('btn-clicked')
+        setTimeout(() => $button.removeClass('btn-clicked'), 200)
+    }
+
+    //keydown events
+
+    $(document).on('keydown', (e) => {
+        let input = e.key;
+        let number = input.match(/\d/)
+        let opMatch = input.match(/[+\-*/]/)
+
+        $flash(number)
+        $flash(opMatch)
+
+        if (input.match(/[enter]/)) {
+            $calculate();
+            $flash('=')
+        }
+
+        if (opMatch != null && $operator != '' && $secondNum != '') {
+            $calculate()
+            $operator = opMatch.input
+        } else if (opMatch != null && $operator === '') {
+            $operator = opMatch.input;
+            $log = $firstNum + $operator
+        }
+
+        if ($operator === '' && number != null) {
+            $firstNum += number;
+            $log = $firstNum;
+        } else if (number != null) {
+            $secondNum += number;
+        }
+
+        $log = $firstNum + $operator + $secondNum
+        $output.html(`<p>${$log}</p>`)
+    })
+
+    //Event listeners for mouse clicks on buttons
+
+    //makes a button look like it's being pressed when clicked on
+    $('button').on('click', function () {
+        $flash(this.value)
     });
 
+    //listening for clicks on numbers, if an operator hasn't been selected then the number is added to the firstNum variable. Else, add it to the secondNum variable.
     $numBtns.on('click', function () {
         if ($operator === '') {
             $firstNum += this.value;
             $log = $firstNum;
         } else {
             $secondNum += this.value;
-            console.log($secondNum)
             $log = $firstNum + $operator + $secondNum;
         }
         $output.html(`<p>${$log}</p>`)
     })
 
+
+    //Checks which operator is being selected, if an operator has already been selected, then calculate the current equation. Otherwise, assign the selected operator to the operator variable.
     $opBtns.on('click', function () {
         if ($operator != '') {
             $calculate();
@@ -79,8 +123,10 @@ $(document).ready(function () {
         $output.html(`<p>${$log}</p>`)
     })
 
+    //listens for click on clear button which will reset all variables.
     $('.clear-btn').on('click', () => $clear())
 
+    //listens for submission/equal button which will perform calculations requested.
     $('form').on('submit', (event) => {
         event.preventDefault();
         $calculate();
