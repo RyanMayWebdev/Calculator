f2b8f38b21f4808ea9bc3e890d4e75c6a07f0b41
Ryan May <ryan777may@gmail.com>
2021-08-03 17:09:14 -0400

    added namespace

--- JavaScript/Calc.js
+++ JavaScript/Calc.js
@@ -1,134 +1,157 @@
-$(document).ready(function () {
+const app = {}
 
-    let $firstNum = '',
-        $secondNum = '',
-        $output = $('#number-log'),
-        $operator = '',
-        $log = '';
+app.$firstNum = '',
+    app.$secondNum = '',
+    app.$output = $('#number-log'),
+    app.$operator = '',
+    app.$log = '';
 
-    const $numBtns = $('.num-btn'),
-        $opBtns = $('.op-btn')
+app.$numBtns = $('.num-btn'),
+    app.$opBtns = $('.op-btn')
 
-    const $add = (num1, num2) => num1 + num2;
+app.$add = (num1, num2) => num1 + num2;
 
-    const $subtract = (num1, num2) => num1 - num2;
+app.$subtract = (num1, num2) => num1 - num2;
 
-    const $multiply = (num1, num2) => num1 * num2;
+app.$multiply = (num1, num2) => num1 * num2;
 
-    const $divide = (num1, num2) => num1 / num2;
+app.$divide = (num1, num2) => num1 / num2;
 
-    const $clear = () => {
-        $firstNum = ''
-        $secondNum = ''
-        $operator = ''
-        $log = ''
-        $output.text($log);
-    };
+app.$clear = () => {
+    app.$firstNum = ''
+    app.$secondNum = ''
+    app.$operator = ''
+    app.$log = ''
+    app.$output.text(app.$log);
+};
 
-    //perform calculation using selected operator and numbers.
-    const $calculate = () => {
-        $firstNum = parseFloat($firstNum);
-        $secondNum = parseFloat($secondNum);
+//perform calculation using selected operator and numbers.
+app.$calculate = () => {
+    app.$firstNum = parseFloat(app.$firstNum);
+    app.$secondNum = parseFloat(app.$secondNum);
 
-        switch ($operator) {
-            case '+':
-                $output.html(`<p>${$add($firstNum, $secondNum)}</p>`);
-                break;
-            case '*':
-                $output.html(`<p>${$multiply($firstNum, $secondNum)}</p>`);
-                break;
-            case '-':
-                $output.html(`<p>${$subtract($firstNum, $secondNum)}</p>`);
-                break;
-            case '/':
-                $output.html(`<p>${$divide($firstNum, $secondNum)}</p>`);
-                break;
+    switch (app.$operator) {
+        case '+':
+            app.$output.html(`<p>${app.$add(app.$firstNum, app.$secondNum)}</p>`);
+            break;
+        case '*':
+            app.$output.html(`<p>${app.$multiply(app.$firstNum, app.$secondNum)}</p>`);
+            break;
+        case '-':
+            app.$output.html(`<p>${app.$subtract(app.$firstNum, app.$secondNum)}</p>`);
+            break;
+        case '/':
+            app.$output.html(`<p>${app.$divide(app.$firstNum, app.$secondNum)}</p>`);
+            break;
 
-            default:
-                break;
-        }
-
-        $firstNum = $output.text()
-        $secondNum = ''
-        $operator = ''
+        default:
+            break;
     }
 
-    const $flash = (value) => {
-        let $button = $("button[value='" + value + "']")
-        $button.addClass('btn-clicked')
-        setTimeout(() => $button.removeClass('btn-clicked'), 200)
-    }
+    app.$firstNum = app.$output.text()
+    app.$secondNum = ''
+    app.$operator = ''
+}
 
-    //keydown events
+app.$flash = (value) => {
+    let $button = $("button[value='" + value + "']")
+    $button.addClass('btn-clicked')
+    setTimeout(() => $button.removeClass('btn-clicked'), 200)
+}
 
+//keydown events
+app.keyDownListner = function () {
     $(document).on('keydown', (e) => {
         let input = e.key;
         let number = input.match(/\d/)
         let opMatch = input.match(/[+\-*/]/)
 
-        $flash(number)
-        $flash(opMatch)
+        app.$flash(number)
+        app.$flash(opMatch)
 
         if (input.match(/[enter]/)) {
-            $calculate();
-            $flash('=')
+            app.$calculate();
+            app.$flash('=')
         }
 
-        if (opMatch != null && $operator != '' && $secondNum != '') {
-            $calculate()
-            $operator = opMatch.input
-        } else if (opMatch != null && $operator === '') {
-            $operator = opMatch.input;
-            $log = $firstNum + $operator
+        if (opMatch != null && app.$operator != '' && app.$secondNum != '') {
+            app.$calculate()
+            app.$operator = opMatch.input
+        } else if (opMatch != null && app.$operator === '') {
+            app.$operator = opMatch.input;
+            app.$log = app.$firstNum + app.$operator
         }
 
-        if ($operator === '' && number != null) {
-            $firstNum += number;
-            $log = $firstNum;
+        if (app.$operator === '' && number != null) {
+            app.$firstNum += number;
+            app.$log = app.$firstNum;
         } else if (number != null) {
-            $secondNum += number;
+            app.$secondNum += number;
         }
 
-        $log = $firstNum + $operator + $secondNum
-        $output.html(`<p>${$log}</p>`)
+        app.$log = app.$firstNum + app.$operator + app.$secondNum
+        app.$output.html(`<p>${app.$log}</p>`)
     })
+}
 
-    //Event listeners for mouse clicks on buttons
+//Event listeners for mouse clicks on buttons
 
-    //makes a button look like it's being pressed when clicked on
+//makes a button look like it's being pressed when clicked on
+app.buttonFlash = function () {
     $('button').on('click', function () {
-        $flash(this.value)
+        app.$flash(this.value)
     });
+}
 
-    //listening for clicks on numbers, if an operator hasn't been selected then the number is added to the firstNum variable. Else, add it to the secondNum variable.
-    $numBtns.on('click', function () {
-        if ($operator === '') {
-            $firstNum += this.value;
-            $log = $firstNum;
+//listening for clicks on numbers, if an operator hasn't been selected then the number is added to the firstNum variable. Else, add it to the secondNum variable.
+app.numButtonListen = function () {
+    app.$numBtns.on('click', function () {
+        if (app.$operator === '') {
+            app.$firstNum += this.value;
+            app.$log = app.$firstNum;
         } else {
-            $secondNum += this.value;
-            $log = $firstNum + $operator + $secondNum;
+            app.$secondNum += this.value;
+            app.$log = app.$firstNum + app.$operator + app.$secondNum;
         }
-        $output.html(`<p>${$log}</p>`)
+        app.$output.html(`<p>${app.$log}</p>`)
     })
+}
 
 
-    //Checks which operator is being selected, if an operator has already been selected, then calculate the current equation. Otherwise, assign the selected operator to the operator variable.
-    $opBtns.on('click', function () {
-        if ($operator != '') {
-            $calculate();
+//Checks which operator is being selected, if an operator has already been selected, then calculate the current equation. Otherwise, assign the selected operator to the operator variable.
+app.operatorButtonListen = function () {
+    app.$opBtns.on('click', function () {
+        if (app.$operator != '') {
+            app.$calculate();
         }
-        $operator = this.value;
-        $log += $operator;
-        $output.html(`<p>${$log}</p>`)
+        app.$operator = this.value;
+        app.$log += app.$operator;
+        app.$output.html(`<p>${app.$log}</p>`)
     })
+}
 
-    //listens for click on clear button which will reset all variables.
-    $('.clear-btn').on('click', () => $clear())
+//listens for click on clear button which will reset all variables.
+app.clearButtonListen = function () {
+    $('.clear-btn').on('click', () => app.$clear())
+}
 
-    //listens for submission/equal button which will perform calculations requested.
+//listens for submission/equal button which will perform calculations requested.
+app.formSubmit = function () {
     $('form').on('submit', (event) => {
         event.preventDefault();
-        $calculate();
+        app.$calculate();
     })
+}
+
+app.init = () => {
+    app.keyDownListner()
+    app.numButtonListen()
+    app.operatorButtonListen()
+    app.formSubmit()
+    app.clearButtonListen()
+    app.buttonFlash()
+}
+
+$(document).ready(function () {
+    app.init()
 })
